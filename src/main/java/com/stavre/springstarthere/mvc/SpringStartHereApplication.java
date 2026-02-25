9653c0d8ed3b562adf53a735adef0388c4f8864e
Stavre <alinstavre@gmail.com>
2026-02-25 23:29:02 +0200

    initial commit

---
+++ src/main/java/com/stavre/springstarthere/mvc/SpringStartHereApplication.java
@@ -0,0 +1,13 @@
+package com.stavre.springstarthere.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartHereApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringStartHereApplication.class, args);
+	}
+
+}
