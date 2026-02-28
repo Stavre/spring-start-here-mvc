3e0a1841ec85c390551a5dc46923378608313edf
Stavre <alinstavre@gmail.com>
2026-02-28 08:29:01 +0200

    fixed pmd issues

--- src/test/java/com/stavre/springstarthere/mvc/controller/ProductControllerTest.java
+++ src/test/java/com/stavre/springstarthere/mvc/controller/ProductControllerTest.java
@@ -1,5 +1,12 @@
 package com.stavre.springstarthere.mvc.controller;
 
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import com.stavre.springstarthere.mvc.model.Product;
 import com.stavre.springstarthere.mvc.service.ProductService;
 import org.junit.jupiter.api.Test;
@@ -8,18 +15,16 @@ import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTe
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// TODO: Find a better way to suppress PMD.UnitTestShouldIncludeAssert
 
 @WebMvcTest(ProductController.class)
 @AutoConfigureRestTestClient
-public class ProductControllerTest {
+@SuppressWarnings("PMD.UnitTestShouldIncludeAssert")
+class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -30,11 +35,13 @@ public class ProductControllerTest {
     @Test
     void productPageShouldReturnProductsViewWithModelAttributes() throws Exception {
 
+        Product firstProduct = new Product(UUID.randomUUID(), "Classic Leather Wallet",
+                "/images/23cf4678.png", new BigDecimal("12.59"));
+        Product secondProduct = new Product(UUID.randomUUID(), "Ceramic Coffee Mug",
+                "/images/3298b269.png", new BigDecimal("12.99"));
+
         // Arrange
-        List<Product> mockProducts = List.of(
-                new Product(UUID.randomUUID(), "Classic Leather Wallet", "/images/23cf4678.png", new BigDecimal("12.59")),
-                new Product(UUID.randomUUID(), "Ceramic Coffee Mug", "/images/3298b269.png", new BigDecimal("12.99"))
-                );
+        List<Product> mockProducts = List.of(firstProduct, secondProduct);
         given(productService.getAllProducts()).willReturn(mockProducts);
 
         // Act & Assert
@@ -42,9 +49,9 @@ public class ProductControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("products.html"))
                 .andExpect(xpath("//div[@id=\"no-products-message\"]").doesNotExist())
-
                 .andExpect(model().attribute("products", mockProducts));
     }
+
     @Test
     void productPageShouldReturnEmptyMessageViewWithModelAttributes() throws Exception {
 
